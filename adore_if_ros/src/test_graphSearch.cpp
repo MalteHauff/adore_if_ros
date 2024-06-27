199eca0055ce6069850e97184daa39dd81e85be6
Hauff, Malte <malte.hauff@dlr.de>
2024-06-27 17:21:04 +0200

    changed data rep

--- adore_if_ros/src/test_graphSearch.cpp
+++ adore_if_ros/src/test_graphSearch.cpp
@@ -37,9 +37,13 @@ namespace adore
                 
                 if(!first_set){
                     first_set=false;
+                    int new_data[msg->info.height*msg->info.width];
+                    for(int i=0; i<msg->info.height*msg->info.width; i++){
+                        new_data[i] = msg->data[i];
+                    }
                     std::cout << typeid(msg->data).name() << '\n';
-                    gs_ = new adore::apps::GraphSearch(adore_if_ros_scheduling::Baseapp::getRosNodeHandle(), (uint32_t)(msg->info.height), (uint32_t)(msg->info.width));//, msg->data, (uint32_t)msg->info.height, (uint32_t)msg->info.width);
-
+                    gs_->init(adore_if_ros_scheduling::Baseapp::getRosNodeHandle(),new_data, msg->info.height, msg->info.width);
+                    //gs_ = new adore::apps::GraphSearch(adore_if_ros_scheduling::Baseapp::getRosNodeHandle(),msg->data, msg->info.height, msg->info.width);
                     // timer callbacks
                     std::function<void()> run_fcn(std::bind(&adore::apps::GraphSearch::update, gs_));
                     adore_if_ros_scheduling::Baseapp::addTimerCallback(run_fcn);
